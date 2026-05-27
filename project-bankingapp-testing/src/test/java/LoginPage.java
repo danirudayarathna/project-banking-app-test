41ccd55158758ee771a2013bb5a4baa4a931371e
Daniru Dayarathna <168005576+danirudayarathna@users.noreply.github.com>
2026-05-27 11:32:04 +0530

    Update LoginPage.java
    new

--- project-bankingapp-testing/src/test/java/LoginPage.java
+++ project-bankingapp-testing/src/test/java/LoginPage.java
@@ -2,8 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
