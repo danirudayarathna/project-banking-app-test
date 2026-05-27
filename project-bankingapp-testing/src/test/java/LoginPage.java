f10a5c272f34502bf2c06e8449e918e10afc1c51
Daniru Dayarathna <168005576+danirudayarathna@users.noreply.github.com>
2026-05-27 11:31:04 +0530

    Fix indentation in LoginPage.java
    Reformat LoginPage.java to normalize indentation and spacing for the class field, methods, and statements. This is a whitespace-only change to improve code readability; no logic or behavior was modified.

--- project-bankingapp-testing/src/test/java/LoginPage.java
+++ project-bankingapp-testing/src/test/java/LoginPage.java
@@ -9,36 +9,36 @@ import org.testng.annotations.Test;
 
 
 public class LoginPage {
-        WebDriver driver;
+    WebDriver driver;
 
-        @BeforeMethod
-        public void loginPageBeforeTest() {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.get("https://parabank.parasoft.com/parabank/index.htm");
-        }
+    @BeforeMethod
+    public void loginPageBeforeTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+    }
 
-        @Test
-        public void loginPageTesting(){
+    @Test
+    public void loginPageTesting(){
             //Test Case 003
-            WebElement userName = driver.findElement(By.xpath("(//input[@name='username'])[1]"));
-            userName.sendKeys("john");
+        WebElement userName = driver.findElement(By.xpath("(//input[@name='username'])[1]"));
+        userName.sendKeys("john");
 
-            WebElement password = driver.findElement(By.xpath("(//input[@name='password'])[1]"));
-            password.sendKeys("demo");
+        WebElement password = driver.findElement(By.xpath("(//input[@name='password'])[1]"));
+        password.sendKeys("demo");
 
-            WebElement loginButton = driver.findElement(By.xpath("(//input[@value='Log In'])[1]"));
-            loginButton.click();
+        WebElement loginButton = driver.findElement(By.xpath("(//input[@value='Log In'])[1]"));
+        loginButton.click();
 
             //Validation
-            String expected = "ParaBank | Accounts Overview";
-            String actual = driver.getTitle();
+        String expected = "ParaBank | Accounts Overview";
+        String actual = driver.getTitle();
 
-            if (expected.equals(actual)){
-                System.out.println("Test is Complete");
-            }else{
-                System.out.println("Test is Failed");
-            }
-
+        if (expected.equals(actual)){
+            System.out.println("Test is Complete");
+        }else{
+            System.out.println("Test is Failed");
         }
+
+    }
 }
